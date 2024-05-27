2eace302458bd5c48e8d4554dbcb42b54255f017
Tiêu Đạt <dattv@ringme.vn>
2024-05-27 11:13:51 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'http',
-            hostname: 'kakoakgames.ringme.vn',
-            port: '',
-            pathname: '/**',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'kakoakgames.ringme.vn',
+        port: '',
+        pathname: '/**',
       },
+    ],
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
